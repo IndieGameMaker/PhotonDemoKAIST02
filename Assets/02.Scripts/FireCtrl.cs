4623937c48699bc905ad873e00244df7a7e61609
Lee JaeHyun <myleje@gmail.com>
2021-02-03 11:26:11 +0900

    .

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class FireCtrl : MonoBehaviour
 {
     public GameObject bullet;
     public Transform firePos;
+    private PhotonView pv;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        pv = GetComponent<PhotonView>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (pv.IsMine == false) return;
+        
         if (Input.GetMouseButtonDown(0))
         {
            Fire(); 
