209877db6213c650a1e7f4d990f32e9d74623a06
Lee JaeHyun <myleje@gmail.com>
2021-02-04 15:31:32 +0900

    .

--- Assets/02.Scripts/TankCtrl.cs
+++ Assets/02.Scripts/TankCtrl.cs
@@ -39,7 +39,7 @@ public class TankCtrl : MonoBehaviour
         float v = Input.GetAxis("Vertical");   // Up, Down Arrow , W, S [-1.0f ~ 0.0f ~ 1.0f]
         float h = Input.GetAxis("Horizontal"); // Left, Right Arrow, A, D [-1.0f ~ 0.0f ~ 1.0f]
 
-        tr.Translate(Vector3.forward * Time.deltaTime * moveSpeed * v);
+        tr.Translate(Vector3.forward * Time.deltaTime * moveSpeed * 0.03f * v);
         tr.Rotate(Vector3.up * Time.deltaTime * turnSpeed * h);        
     }
 }
